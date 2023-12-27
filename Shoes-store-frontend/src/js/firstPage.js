334955882b838457f4f23e9d1ef288116af13697
Mahdi Eslami <eslami.mahdi.98@gmail.com>
2023-12-27 18:46:48 +0330

    update first page html and js

--- Shoes-store-frontend/src/js/firstPage.js
+++ Shoes-store-frontend/src/js/firstPage.js
@@ -1,5 +1,22 @@
+"use strict";
 const firstPage = document.getElementById("FP");
 
-firstPage.addEventListener("click", () => {
-  location.href = "/slider";
-});
+setTimeout(() => {
+  firstPage.innerHTML = `<div class="relative h-full w-full bg-hero">
+  <div
+    id="alert"
+    class="absolute top-0 h-full w-full bg-gradient-to-t from-black to-transparent"
+  ></div>
+  <div class="absolute bottom-0 w-full px-4 text-white">
+    <p class="pb-8 text-5xl font-medium">Welcome to 👋</p>
+    <p class="pb-8 text-6xl font-semibold">Shoea</p>
+    <p class="pb-10 font-medium">
+      The best sneakers & shoes e-commerse app of the century for your
+      fashion needs!
+    </p>
+  </div>
+</div>`;
+  firstPage.addEventListener("click", () => {
+    location.href = "/slider";
+  });
+}, 3000);
